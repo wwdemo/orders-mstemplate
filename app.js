d7f9120fd7fb6c1101df0750fb2e06e47b9901d7
cjohannessen <cjohannessen@us.ibm.com>
2017-02-15 07:47:20 -0600

    fixes

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 /*globals cloudantService:true */
 /*eslint-env node */
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var PORT = 80;
 
 var ordersDb = [];
 
-//Setup Cloudant Service.
-//cloudantService = appEnv.getService("myMicroservicesCloudant");
-
 //Setup middleware.
 var app = express();
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //REST HTTP Methods
 app.post('/rest/orders', function(req, res) {
@@ -19,7 +18,6 @@ app.post('/rest/orders', function(req, res) {
 	res.status(201).send({msg: 'Successfully created item'});
 	}
 );
-//app.get('/rest/orders/:id', orders.find);
 app.get('/rest/orders', function(req, res){
 	res.status(201).send('Orders: ' + ordersDb.toString());
 	}
